45fc7dcb7cc5e80c06828bd736b6ede896463cba
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-10 19:38:08 -0800

    add engine configuration to package.json file and set up folders for components

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
+import About from '../about';
+import Contact from '../contact';
+import Landing from '../landing';
+import Projects from '../projects';
 
 class App extends React.Component{
   render(){
-    return('blah');
+    return(
+      <BrowserRouter>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/projects" component={Projects}/>
+        <Route exact path="/contact" component={Contact}/>
+      </BrowserRouter>
+    );
   }
 }
 
