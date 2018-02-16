4a99476de9a6d0ab6c25217428a53d5d5f4d9098
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-16 13:35:17 -0800

    remove logs

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(express.static(`${__dirname}/frontend/src/build`));
 
 app.get('*', (request, response) => {
-  console.log('hello');
-  console.log(`${__dirname}/frontend/src/build/index.html`)
   response.sendFile(`${__dirname}/frontend/src/build/index.html`);
 })
 
