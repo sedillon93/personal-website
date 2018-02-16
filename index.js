d13c9b51a62a158af113c95f04fc10b2b135d752
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-16 13:25:50 -0800

    trying something for index.js sending html file

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(`${__dirname}/frontend/src/build`));
+
+app.get('*', (request, response) => {
+  response.sendFile(`${__dirname}/frontend/src/build/index.html`);
+})
+
+app.listen(process.env.PORT);
