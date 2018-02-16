52d233052e3d6ea4c8685a1046f2c9d89e28f67b
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-16 13:30:20 -0800

    messing around with logs in index.js to see what prints to page

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.static(`${__dirname}/frontend/src/build`));
 
 app.get('*', (request, response) => {
   response.sendFile(`${__dirname}/frontend/src/build/index.html`);
+  console.log(`${__dirname}/frontend/src/build/index.html`)
+  console.log('hello');
 })
 
 app.listen(process.env.PORT);
