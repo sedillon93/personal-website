473d671d6357a387b61da9006c2eae689f9d731d
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-16 12:49:21 -0800

    navbar inline, styled; reset applied

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import '../reset.scss';
 
 const container = document.createElement('div');
 document.body.appendChild(container);
