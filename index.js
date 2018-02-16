6834b6e9b2280898f737ed4c7d27603edef0c2d0
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-16 13:31:39 -0800

    still messing with logs

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static(`${__dirname}/frontend/src/build`));
 
 app.get('*', (request, response) => {
-  response.sendFile(`${__dirname}/frontend/src/build/index.html`);
-  console.log(`${__dirname}/frontend/src/build/index.html`)
   console.log('hello');
+  console.log(`${__dirname}/frontend/src/build/index.html`)
+  response.sendFile(`${__dirname}/frontend/src/build/index.html`);
 })
 
 app.listen(process.env.PORT);
