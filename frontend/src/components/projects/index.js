fbc4524eec531480748253e5d21d19adcc18886c
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-17 09:36:21 -0800

    adding conditional rendering for project description

--- frontend/src/components/projects/index.js
+++ frontend/src/components/projects/index.js
@@ -1,34 +1,65 @@
 import React from 'react';
-
 /*
-1. on click the 'here' spans should slide out the project description & change text to 'hide'
+1. on click the 'here' spans should slide out the project description & change clickable text to 'hide'
 2. replace image tags with project screenshot
 */
 class Projects extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      project1Visible: false,
+      project2Visible: false,
+      project3Visible: false,
+    };
+    this.handleLearnMoreClick = this.handleLearnMoreClick.bind(this);
+  }
+
+  handleLearnMoreClick(event){
+    let target = event.target.id;
+    let statePropertyToChange = `${target}Visible`;
+    // toggle whether projectX visibility is true or false;
+    this.setState(statePropertyToChange = !statePropertyToChange)
+  }
+
   render(){
+    let project1JSX = {
+      <p>Description for project 1</p>
+      <a href="">Github link</a>
+    };
+
+    let project2JSX = {
+      <p>Description for project 2</p>
+      <a href="">Github link</a>
+    };
+
+    let project3JSX = {
+      <p>Description for project 3</p>
+      <a href="">Github link</a>
+    };
+
     return (
       <section>
         <div>
           <h4>Project 1 title</h4>
           <img src="" />
-          <p>Click <span>here</span> to learn more</p>
-          <p>Description for project 1</p>
-          <a href="">Github link</a>
+          <p id="project1">Learn more</p>
+          {project1Visible ? project1JSX : undefined}
         </div>
+
         <div>
           <h4>Project 2 title</h4>
           <img src="" />
-          <p>Click <span>here</span> to learn more</p>
-          <p>Description for project 2</p>
-          <a href="">Github link</a>
+          <p id="project2">Learn more</p>
+          {project2Visible ? project2JSX : undefined}
         </div>
+
         <div>
           <h4>Project 3 title</h4>
           <img src="" />
-          <p>Click <span>here</span> to learn more</p>
-          <p>Description for project 3</p>
-          <a href="">Github link</a>
+          <p id="project3">Learn more</p>
+          {project3Visible ? project3JSX : undefined}
         </div>
+
       </section>
     );
   }
