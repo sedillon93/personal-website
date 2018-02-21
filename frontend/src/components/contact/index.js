56664de444fe8ad8ed30c007ab60aea863b53d5d
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-20 20:33:48 -0800

    contact info included; email link creates new email with my username as recipient

--- frontend/src/components/contact/index.js
+++ frontend/src/components/contact/index.js
@@ -5,8 +5,12 @@ class Contact extends React.Component{
     return (
       <section>
         <p>Want to get in touch?</p>
-        <div>Email</div>
-        <div>LinkedIn</div>
+        <div>
+          <span>Email: </span><a href="mailto:sedillon93@gmail.com">sedillon93@gmail.com</a>
+        </div>
+        <div>
+          <span>LinkedIn: </span><a href="https://www.linkedin.com/in/shannon-e-dillon/">Shannon E Dillon</a>
+        </div>
       </section>
     );
   }
