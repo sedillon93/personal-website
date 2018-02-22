39a82fa740472de2d617f2ca409b6479ea5942ff
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-22 11:09:34 -0800

    revise Links for navbar; now using scrollIntoView to move around page and it looks much better

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 class Navbar extends React.Component{
+  constructor(props){
+    super(props);
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event){
+    event.preventDefault();
+    let target = event.target.id;
+    let sectionToScrollTo = document.getElementById(`${target}Section`);
+    // console.log(sectionToScrollTo);
+    sectionToScrollTo.scrollIntoView(true);
+
+  }
+
   render(){
     return (
       <nav>
         <ul>
-          <li id='homeLinkIcon'><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/projects'>Projects</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
+          <li id='homeLinkIcon'><button onClick={this.scrollToSection} id="home">Home</button></li>
+          <li><button id="contactInfo" onClick={this.scrollToSection}>Contact</button></li>
+          <li><button id="project" onClick={this.scrollToSection}>Projects</button></li>
+          <li><button id="about" onClick={this.scrollToSection}>About</button></li>
         </ul>
       </nav>
     );
@@ -17,3 +31,8 @@ class Navbar extends React.Component{
 }
 
 export default Navbar;
+
+// <li id='homeLinkIcon'><Link to='/'>Home</Link></li>
+// <li><Link to='/contact'>Contact</Link></li>
+// <li><Link to='/projects'>Projects</Link></li>
+// <li><Link to='/about'>About</Link></li>
