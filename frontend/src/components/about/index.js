0585ad31b845a4c505f493a1dcb7a8754083098f
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-21 17:13:35 -0800

    remove margin from body (looked bad); remove headshot (looked bad); about me blurb now in place of headshot

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -9,11 +9,10 @@ import './_about.scss';
 class About extends React.Component{
   render(){
     return (
-      <section>
-        <p>Hello There</p>
+      <section id="aboutSection">
+        <p id="greeting">Hello There</p>
         <img src="/frontend/src/vendor/background-image.jpg" />
-        <p>I'm Shannon. Full-stack JavaScript developer by day, avid bookworm by night, and kickboxer by the occassional weekend. Keep exploring if you'd like to learn more about me, my background, and the projects I'm working on.</p>
-        <img src="/frontend/src/vendor/shannon_profile.jpg"/>
+        <p id="aboutMeBlurb">Shannon here. </p>
         <PersonalBackground />
       </section>
     );
