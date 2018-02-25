040cbb1cf739d98a948524b69607f1b09c2e8727
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-25 12:35:01 -0800

    correcting absolute path for index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('*', (request, response) => {
-  response.sendFile(`personal-website/dist/index.html`);
+  response.sendFile(`/personal-website/dist/index.html`);
 })
 
 app.listen(process.env.PORT);
