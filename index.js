487fa622d597d8a022ba8e8418a6015d1d9728d6
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-24 19:34:34 -0800

    changing server route to index.html page to fix 404 (hopefully)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('*', (request, response) => {
-  response.sendFile(`${__dirname}/dist/index.html`);
+  response.sendFile(`dist/index.html`);
 })
 
 app.listen(process.env.PORT);
