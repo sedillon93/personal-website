20cd968271cb9b84156e066ecf2e9fbbc1a6c564
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-25 12:44:31 -0800

    testing app configuration

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const testing = express();
 
-app.use(express.static(`${__dirname}/dist`));
+testing.use(express.static(`${__dirname}/dist`));
 
-app.get('*', (request, response) => {
+testing.get('*', (request, response) => {
   response.sendFile(`index.html`, {root: './dist'});
 })
 
-app.listen(process.env.PORT);
+testing.listen(process.env.PORT);
