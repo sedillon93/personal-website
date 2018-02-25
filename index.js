4596c38dfb2d1609c909cf0faba1139d80cc40e9
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-25 12:39:16 -0800

    fixing root location for index.js sendFile

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('*', (request, response) => {
-  response.sendFile(`dist/index.html`, {root: '.'});
+  response.sendFile(`dist/index.html`, {root: 'personal-website'});
 })
 
 app.listen(process.env.PORT);
